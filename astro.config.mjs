4e2bdc17d3d3a061d222d0574262253819cbe386
Keiber Urbila <keiberup.dev@gmail.com>
2024-02-14 09:36:54 -0400

    fix: SSR changed to server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid",
+  output: "server",
   adapter: node({
     mode: "standalone"
   })
